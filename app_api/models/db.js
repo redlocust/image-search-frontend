790c9bfb6ecf8aa2c505fa627097619999511dd5
Кузнецов <@IS.local>
2017-04-06 15:19:33 +0300

    fix

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,20 +1,20 @@
-var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/imagesearch');
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Connection error:'));
-db.once('open', () => {
-    console.log("We connected");
-});
-
-
-// term: query text, when: when query was
-var querySchema = mongoose.Schema({
-    term: String,
-    when: String
-});
-
-var Query = mongoose.model('Query', querySchema);
-
-module.exports = Query;
+// var mongoose = require('mongoose');
+//
+// mongoose.connect('mongodb://localhost:27017/imagesearch');
+//
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'Connection error:'));
+// db.once('open', () => {
+//     console.log("We connected");
+// });
+//
+//
+// // term: query text, when: when query was
+// var querySchema = mongoose.Schema({
+//     term: String,
+//     when: String
+// });
+//
+// var Query = mongoose.model('Query', querySchema);
+//
+// module.exports = Query;
