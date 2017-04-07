e2b41503b36411eb1e3cd9fef4d06e6ec92f7ac5
Кузнецов <@IS.local>
2017-04-07 10:37:14 +0300

    fix port

--- server.js
+++ server.js
@@ -5,14 +5,11 @@ var mongoose = require('mongoose');
 
 var apiRouter = require('./app_api/routes/main');
 
-var port = process.env.PORT || 3000;
 
-app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url);
-  } else {
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  }
 });
 
 app.use(express.static(path.join(__dirname, '/public')));
@@ -20,6 +17,8 @@ app.use('/api', express.static(path.join(__dirname, '/app_api')));
 
 app.use('/api', apiRouter);
 
+var port = process.env.PORT || 3000;
+
 app.listen(port, () => {
-  console.log('Server started on port: ' + port);
+    console.log('Server started on port: ' + port);
 });
